83b3a425f9e502602f25f2ede95d3e27ebe037b9
Nestor Mamani <nmamanipantoja@gmail.com>
2023-03-28 06:54:12 -0500

    pokemon type in contract

--- contract/contracts/PokemonFactory.sol
+++ contract/contracts/PokemonFactory.sol
@@ -3,6 +3,26 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PokemonFactory {
+    enum PokemonType {
+        Fire,
+        Water,
+        Grass,
+        Electric,
+        Psychic,
+        Normal,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Rock,
+        Bug,
+        Ghost,
+        Dragon,
+        Dark,
+        Steel,
+        Fairy
+    }
+
     struct Skill {
         string name;
         string description;
@@ -11,7 +31,8 @@ contract PokemonFactory {
     struct Pokemon {
         uint id;
         string name;
-        mapping(uint => Skill) skills;
+        Skill[] skills;
+        PokemonType[] pokemonType;
     }
 
     Pokemon[] private pokemons;
@@ -24,7 +45,8 @@ contract PokemonFactory {
     function createPokemon(
         string memory _name,
         uint _id,
-        Skill[] memory _skills
+        Skill[] memory _skills,
+        PokemonType[] memory _pokemonType
     ) public {
         require(
             ownerPokemonCount[msg.sender] == 0,
@@ -36,6 +58,10 @@ contract PokemonFactory {
             "Name must be at least 3 characters long"
         );
         require(_skills.length > 0, "Pokemon must have at least 1 skill");
+        require(
+            _pokemonType.length > 0,
+            "Pokemon must have at least 1 pokemon type"
+        );
 
         Pokemon storage newPokemon = pokemons[_id];
         newPokemon.id = _id;
@@ -43,6 +69,10 @@ contract PokemonFactory {
 
         for (uint i = 0; i < _skills.length; i++) {
             newPokemon.skills[i] = _skills[i];
+        }
+
+        for (uint i = 0; i < _pokemonType.length; i++) {
+            newPokemon.pokemonType[i] = _pokemonType[i];
         }
 
         pokemonToOwner[_id] = msg.sender;
